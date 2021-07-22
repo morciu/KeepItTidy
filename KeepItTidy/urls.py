6c9f8dc4469b12d87ea31e9af3f0e99b1e6e0fff
morciu <marianpralea@gmail.com>
2021-07-22 18:22:40 +0300

    Frontend for login form

--- KeepItTidy/urls.py
+++ KeepItTidy/urls.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 	path("", views.index, name="index"),
+	path("login", views.login, name="login")
 	]
 
 urlpatterns += staticfiles_urlpatterns()
--- KeepItTidy/views.py
+++ KeepItTidy/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "keepittidy/index.html")
+
+
+def login(request):
+	return render(request, "keepittidy/login.html")
