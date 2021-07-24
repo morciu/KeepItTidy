65121e6e9a116322de5cdbac76d9ffb54d10feff
morciu <marianpralea@gmail.com>
2021-07-24 19:59:49 +0300

    New users can register

--- KeepItTidy/views.py
+++ KeepItTidy/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
+
+from .models import User
+
 
 # Create your views here.
 
@@ -11,4 +17,25 @@ def login(request):
 
 
 def register(request):
-	return render(request, "keepittidy/register.html")
+	if request.method == "POST":
+		username = request.POST["inputUser"]
+		password = request.POST["inputPassword"]
+		re_password = request.POST["inputRePassword"]
+
+		# Check if password and re_password match
+		if password != re_password:
+			return render(request, "keepittidy/register.html", {"error_msg": "Make sure the passwords match!"})
+
+		# Try to create new user
+		try:
+			user = User.objects.create_user(username=username, password=password)
+			user.save()
+		except IntegrityError:
+			return render(request, "keepittidy/register.html", {"error_msg": "Sorry, username already taken by someone else."})
+
+		return HttpResponseRedirect(reverse("index"))
+
+
+
+	else:
+		return render(request, "keepittidy/register.html")
