712bec51e82585bfebbdd914fa55555c4d7d5753
morciu <marianpralea@gmail.com>
2021-07-25 11:59:54 +0300

    User can log in

--- KeepItTidy/urls.py
+++ KeepItTidy/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 	path("", views.index, name="index"),
-	path("login", views.login, name="login"),
+	path("login", views.login_view, name="login_view"),
 	path("register", views.register, name="register")
 	]
 
--- KeepItTidy/views.py
+++ KeepItTidy/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
 
 from .models import User
 
@@ -12,8 +13,23 @@ def index(request):
 	return render(request, "keepittidy/index.html")
 
 
-def login(request):
-	return render(request, "keepittidy/login.html")
+def login_view(request):
+	if request.method == "POST":
+		# Attempt to sign user in
+		username = request.POST["inputUser"]
+		password = request.POST["inputPassword"]
+		user = authenticate(request, username=username, password=password)
+
+		# Check if authenticated
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("index"))
+		else:
+			return render(request, "keepittidy/login.html", {
+				"error_msg": "Invalid login!"
+				})
+	else:
+		return render(request, "keepittidy/login.html")
 
 
 def register(request):
@@ -24,7 +40,9 @@ def register(request):
 
 		# Check if password and re_password match
 		if password != re_password:
-			return render(request, "keepittidy/register.html", {"error_msg": "Make sure the passwords match!"})
+			return render(request, "keepittidy/register.html", {
+				"error_msg": "Make sure the passwords match!"
+				})
 
 		# Try to create new user
 		try:
@@ -32,10 +50,7 @@ def register(request):
 			user.save()
 		except IntegrityError:
 			return render(request, "keepittidy/register.html", {"error_msg": "Sorry, username already taken by someone else."})
-
+		# Redirect to index page
 		return HttpResponseRedirect(reverse("index"))
-
-
-
 	else:
 		return render(request, "keepittidy/register.html")
