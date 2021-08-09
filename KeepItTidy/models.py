8db99fc9fc1d60f4f6437db3f95a4a3511d74611
morciu <marianpralea@gmail.com>
2021-08-09 10:55:53 +0300

    Created Collection Model and TextField Model

--- KeepItTidy/models.py
+++ KeepItTidy/models.py
@@ -4,3 +4,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
 	pass
+
+
+class Collection(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="collection")
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+
+
+class TextField(models.Model):
+	text = models.CharField(max_length=500)
