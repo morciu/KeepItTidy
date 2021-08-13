145a0b13837a59489f1b90561314804f4add15b1
morciu <marianpralea@gmail.com>
2021-08-13 21:28:02 +0300

    add new field button and only prints a debug message at the moment

---
+++ KeepItTidy/static/KeepItTidy/keepItTidy.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+	// Add Field Button
+	document.querySelector("#newField").addEventListener('click', addNewField);
+});
+
+function addNewField() {
+	console.log("pressed add new field button");
+}
