ffbeeb2bec0c41f8803aa7df0bfe7d7d9f3ed3b9
morciu <marianpralea@gmail.com>
2021-08-15 20:36:33 +0300

    Frontend - Create new field forms - does not fit on screen if too many

--- KeepItTidy/static/KeepItTidy/keepItTidy.js
+++ KeepItTidy/static/KeepItTidy/keepItTidy.js
@@ -1,8 +1,35 @@
+let numberOfFields = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
+	
 	// Add Field Button
 	document.querySelector("#newField").addEventListener('click', addNewField);
 });
 
 function addNewField() {
-	console.log("pressed add new field button");
+	// Cloning first field form to create new ones and modifying new id and name for each one
+	
+	numberOfFields += 1;
+
+	let extraForm = document.querySelector("#fieldForm1").cloneNode(true);
+	extraForm.id = "fieldForm" + numberOfFields
+	let extraFormChildren = extraForm.childNodes;
+	extraFormChildren.forEach(function(item) {
+		if (item.nodeName == "DIV") {
+			let itemChildren = item.childNodes;
+			itemChildren.forEach(function(child) {
+				if (child.nodeName == "INPUT") {
+					child.id = "fieldName" + numberOfFields;
+					child.setAttribute("name", "fieldName" + numberOfFields);
+				}
+				if (child.nodeName == "SELECT") {
+					child.id = "fieldType" + numberOfFields;
+					child.setAttribute("name", "fieldType" + numberOfFields);
+				}
+			});
+		}
+	});
+
+	let fieldFormsDiv = document.querySelector("#fieldForms");
+	fieldFormsDiv.appendChild(extraForm);
 }
